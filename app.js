dbe5e07bc23038336fd84efeb43515725a21f5bd
Bryaan159 <00057720@uca.edu.sv>
2022-05-20 01:14:03 -0600

    Agregando la API el laboratorio

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var helmet = require("helmet");
+require("./database/config");
+//Variables agregadas
+var userRouter = require("./routers/user");
+var wordRouter = require("./routers/word");
+var authRouter = require("./routers/auth");
+var passport = require("passport");
+
+require("./auth/auth");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(authRouter);
+
+app.use(passport.authenticate("jwt", { session: false }))
+app.use("/user", userRouter);
+app.use("/word", wordRouter);
+
+
+
+module.exports = app;
